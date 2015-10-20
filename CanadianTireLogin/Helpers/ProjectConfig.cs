1f731c368f69c46ee650223bcf2f410e195a0424
Yaroslav Pashkov <yaroslav_pashkov@epam.com>
2015-10-20 23:17:09 +0300

    Minor changes

---
+++ CanadianTireLogin/Helpers/ProjectConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace CanadianTireLogin.Helpers
+{
+    public class ProjectConfig
+    {
+        public static String Login = ConfigurationManager.AppSettings["Login"];
+        public static String Password = ConfigurationManager.AppSettings["Login"];
+    }
+}
