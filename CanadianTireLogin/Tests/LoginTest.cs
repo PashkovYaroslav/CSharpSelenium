a5cb6c71566093948499431633b7f9d50b0e2366
PashkovYaroslav <generalyaro@gmail.com>
2015-10-21 10:58:59 +0300

    Update

--- CanadianTireLogin/Tests/LoginTest.cs
+++ CanadianTireLogin/Tests/LoginTest.cs
@@ -2,7 +2,6 @@
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using CanadianTireLogin.Pages;
